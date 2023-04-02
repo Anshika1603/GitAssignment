7f07154af19c3121e20af395523424cd2f5f21f8
Anshika1603 <anshika.srivastava@knoldus.com>
2023-04-02 21:44:40 +0530

    Adding Person Class File

---
+++ src/main/java/com/knoldus/Person.java
@@ -0,0 +1,34 @@
+package com.knoldus;
+
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person() {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public boolean isAdult(Integer age){
+        if(age>=18)
+            return true;
+        else
+            return false;
+    }
+}
